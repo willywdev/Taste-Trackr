ecbedd175abd0fc17662cd833106a25a018a4475
Wilhelm Werwein <werwein.dev@gmail.com>
2023-08-11 23:36:17 +0200

    add firebase and add forms and styling

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
-const contentBox = document.querySelector('[data-js="content"]');
-console.log(contentBox);
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { registerForm } from "./createForms";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDOqXT90KaOECDCxOx-AZagJHG6NsWFIsY",
+  authDomain: "taste-trackr-d6c61.firebaseapp.com",
+  projectId: "taste-trackr-d6c61",
+  storageBucket: "taste-trackr-d6c61.appspot.com",
+  messagingSenderId: "736616958411",
+  appId: "1:736616958411:web:61bd0fe536cd97eb1e0e4e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const contentBox = document.querySelector('[data-js="content"]');
+const registerElement = document.querySelector('[data-js="registerElement"]');
+
+registerElement.addEventListener("click", () => {
+  registerForm();
+});
---
+++ js/user.js
@@ -0,0 +1,16 @@
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+const auth = getAuth(app);
+
+function registerUser(email, password) {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
+}
