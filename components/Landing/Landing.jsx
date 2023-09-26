6f945e163609c915ca2b1c31176741284ca56e0f
Wilhelm Werwein <werwein.dev@gmail.com>
2023-09-26 15:26:04 +0200

    edit Landing Text

--- components/Landing/Landing.jsx
+++ components/Landing/Landing.jsx
@@ -1,15 +1,36 @@
+import styled from "styled-components";
+import { StyledLink } from "../StyledLink/Link.styled";
 import { StyledSection } from "../StyledSection/Section.styled";
 
 export default function Landing() {
   return (
     <StyledSection>
-      <h2>Hello World!</h2>
+      <h2>Welcome to Taste Trackr 👨🏻‍🍳</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis
-        numquam tempora veniam alias illo. Tempore id illo, natus molestias
-        fugiat dolorem ipsam aliquid quae vitae voluptate enim perspiciatis nisi
-        soluta?
+        Track your personal ratings of the restaurants you visit or order often.
+      </p>
+      <StyledList>
+        <li>✅ Nobody sees your ratings. They are just for you!</li>
+        <li>
+          ✅ Use the search to quickly find a restaurant you rated before.
+        </li>
+        <li>✅ Tried a restaurant again and found it better? Update it 👍🏻</li>
+      </StyledList>
+      <p>
+        Crafted together with a lot 🐈 by{" "}
+        <StyledLink href="https://github.com/willywdev" target="_blank">
+          willywdev
+        </StyledLink>
+        .
       </p>
     </StyledSection>
   );
 }
+
+const StyledList = styled.ul`
+  list-style-type: none;
+
+  li {
+    margin-bottom: 2px;
+  }
+`;
