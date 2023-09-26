c604ea9a98026152cfd2e5ba3ee6518b8cd392af
Wilhelm Werwein <werwein.dev@gmail.com>
2023-09-26 09:36:48 +0200

    add useRouter and useEffect to forward user to restaurants page, if logged in

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
 import GlobalStyle from "@/GlobalStyle";
 import Header from "@/components/Header/Header";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  function handleLoggedIn() {
+    setIsLoggedIn(!isLoggedIn);
+  }
+
   return (
     <>
       <GlobalStyle />
       <Header />
-      <Component {...pageProps} />
+      <Component {...pageProps} isLoggedIn={isLoggedIn} />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
 import Landing from "@/components/Landing/Landing";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-export default function Home() {
+export default function Home({ isLoggedIn }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      router.push("/restaurants");
+    }
+  }, []);
+
   return (
     <main>
       <Landing />
